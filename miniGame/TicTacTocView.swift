46a4bac3360942e50037d305da869fd816aeeb76
james <junyoungcat@gmail.com>
2021-08-28 22:46:30 +0900

    Create tic tac toc mini game view

---
+++ miniGame/TicTacTocView.swift
@@ -0,0 +1,20 @@
+//
+//  TicTacTocView.swift
+//  miniGame
+//
+//  Created by james on 2021/08/28.
+//
+
+import SwiftUI
+
+struct TicTacTocView: View {
+    var body: some View {
+        Text("TIC TAC TOC")
+    }
+}
+
+struct TicTacTocView_Previews: PreviewProvider {
+    static var previews: some View {
+        TicTacTocView()
+    }
+}
