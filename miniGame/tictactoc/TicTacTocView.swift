8650c066dc9bf903b08b976cfdd363fb2570b154
james <junyoungcat@gmail.com>
2021-08-29 17:01:35 +0900

    Add bingo circle board

--- miniGame/tictactoc/TicTacTocView.swift
+++ miniGame/tictactoc/TicTacTocView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct TicTacTocView: View {
     
+    @StateObject private var vieModel = GameViewModel()
     @State private var selectedCircleColor: GameColor = .red
     @State private var selectedLevel: Level = .easy
     
@@ -17,10 +18,48 @@ struct TicTacTocView: View {
             VStack{
                 PickerColorView(selectedCircleColor: $selectedCircleColor)
                 PickerLevelView(selectedLevel: $selectedLevel)
+                
+                LazyVGrid(columns: vieModel.columns, spacing: 5) {
+                    ForEach(0..<9) {i in
+                        ZStack{
+                            GameSquareView(proxy: geometry, selectedSide: selectedCircleColor)
+                        }
+                    }
+                }
             }
+            
+            
         }
         .navigationBarTitleDisplayMode(.inline)
         .navigationBarTitle("TIC TAC TOC")        
+    }
+}
+
+struct CircleBoard: View {
+    var proxy: GeometryProxy
+    var color: Color
+    
+    var body: some View {
+        let number = proxy.size.width/3 - 15
+        Circle()
+            .foregroundColor(color)
+            .frame(width: number, height: number, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GameSquareView: View {
+    var proxy: GeometryProxy
+    var selectedSide: GameColor
+    
+    var body: some View {
+        switch selectedSide {
+        case .red:
+            CircleBoard(proxy: proxy, color: Color.red)
+        case .green:
+            CircleBoard(proxy: proxy, color: Color.green)
+        case .blue:
+            CircleBoard(proxy: proxy, color: Color.blue)
+        }
     }
 }
 
