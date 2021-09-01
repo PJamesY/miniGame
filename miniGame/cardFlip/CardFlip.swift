e763d56c42e649cdd9624c8117c99fa4b9903b58
james <junyoungcat@gmail.com>
2021-09-02 00:13:04 +0900

    Refactoring card flip logic

--- miniGame/cardFlip/CardFlip.swift
+++ miniGame/cardFlip/CardFlip.swift
@@ -19,17 +19,10 @@ struct CardFlip: View {
                 ForEach(0..<cardFlipModel.row) { row in
                     HStack(alignment:.center) {
                         ForEach(0..<cardFlipModel.col) { col in
-//                            CardForFlip(geometry:geometry, answer: cardFlipModel.colors[cardFlipModel.shuffledIndex[row + (col * 3) + col]], canbefliped: cardFlipModel.canBeFliped, totalFlipedCount: cardFlipModel.flipedCount, flip: cardFlipModel.flipCard )
-                            
                             CardForFlip(geometry:geometry, cf: cardFlipModel, idx:row + (col * 3) + col)
-                                
                         }
-                        
                     }
-                    
                 }
-                
-                
             }
             .frame(width: geometry.size.width, height: geometry.size.height, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
             
@@ -44,12 +37,6 @@ struct CardForFlip: View {
     var geometry: GeometryProxy
     var cf: CardFlipModel
     var idx: Int
-//    var answer: Color
-//    var canbefliped: Bool
-//    var totalFlipedCount: Int
-//    var flip: () -> Void
-    @State var flipped = false // state variable used to update the card
-//    @StateObject var cardFlipModel1: CardFlipModel
     
     var body: some View {
         RoundedRectangle(cornerRadius: 20)
@@ -58,27 +45,9 @@ struct CardForFlip: View {
             .rotation3DEffect(cf.colors[cf.shuffledIndex[idx]].flipped ? Angle(degrees: 180): Angle(degrees: 0), axis: (x: CGFloat(0), y: CGFloat(10), z: CGFloat(0)))
             .animation(.default) // implicitly applying animation
             .onTapGesture {
-//                if (cf.canBeFliped) {
                     cf.flipCard(for: idx)
-//                }
-                
-//                cf.colors[cf.shuffledIndex[idx]].flipped.toggle()
-//                delay()
-//                if (cardFlipModel.colors[idx].flipped) {
-//                    cardFlipModel.colors[idx].flipped.toggle()
-//                    delay()
-//                }
-//                if (totalFlipedCount == 1) {
-//                    delay()
-//                }
                 
             }
-    }
-    
-    private func delay()  {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            cf.colors[cf.shuffledIndex[idx]].flipped = false
-        }
     }
 }
 
--- miniGame/cardFlip/CardFlipModel.swift
+++ miniGame/cardFlip/CardFlipModel.swift
@@ -10,7 +10,6 @@ import SwiftUI
 final class CardFlipModel: ObservableObject {
     @Published var row = 4
     @Published var col = 4
-//    @Published var colors:[Color] = [Color.red, Color.red, Color.blue, Color.blue, Color.green, Color.green, Color.yellow, Color.yellow, Color.orange, Color.orange, .purple, .purple, .black, .black, .pink, .pink]
     
     @Published var colors: [(color:Color, answer:Int, flipped:Bool, correct:Bool)] = [
         (.red, 1, false, false), (.red, 1, false, false), (.blue, 2, false, false), (.blue, 2, false, false), (.green, 3, false, false), (.green, 3, false, false),
@@ -21,91 +20,55 @@ final class CardFlipModel: ObservableObject {
     @Published var canBeFliped = true
     var compareIdx: Int = 1
     
-//    func checkAnswer () -> Bool {
-//
-//    }
-    
-    private func delay(idx:Int)  {
+    private func unflip(idx:Int, isCanBeFliped: Bool)  {
         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
             self.colors[self.shuffledIndex[idx]].flipped = false
+            if (isCanBeFliped) {
+                self.canBeFliped = true
+                self.flipedCount = 0
+            }
+            
+        }
+    }
+    
+    private func delay () {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.canBeFliped = true
+            self.flipedCount = 0
         }
     }
     
     func flipCard (for idx:Int) {
-//        if
-        print("INDEX", idx)
         if (!colors[shuffledIndex[idx]].correct && canBeFliped) {
-            colors[shuffledIndex[idx]].flipped = true
+            
             flipedCount += 1
             
             if (flipedCount == 1) {
-                print("1개 뒤집힘")
-                compareIdx = idx
+                firstCardFlipped(idx: idx)
             } else if (flipedCount == 2) {
-                canBeFliped = false
-                if (colors[shuffledIndex[idx]].answer == colors[shuffledIndex[compareIdx]].answer) {
-                    print("정답!!!")
-                    colors[shuffledIndex[idx]].correct = true
-                    colors[shuffledIndex[compareIdx]].correct = true
-                } else {
-                    print("틀림1!!")
-                    delay(idx: idx)
-                    delay(idx: compareIdx)
-//                    canBeFliped = true
-                }
-                canBeFliped = true
-                flipedCount = 0
+                secondCardFlipped(idx: idx)
             }
         }
         
         
-//        if (flipedCount == 1) {
-//            print("1개 뒤집힘")
-//            compareIdx = idx
-//        } else if (flipedCount == 2) {
-//            print("2개 뒤집힘")
-//            canBeFliped = false
-//            delay(idx: idx)
-//            delay(idx: compareIdx)
-////            canBeFliped = true
-//            flipedCount = 0
-//        }
-//        if (flipedCount == 1) {
-////            checkSheet.append(idx)
-//
-//            compareIdx = idx
-//        }
-//        else if (flipedCount == 2) {
-//            print("compare", compareIdx, idx)
-//            if (colors[shuffledIndex[compareIdx]].answer == colors[shuffledIndex[idx]].answer) {
-//                colors[shuffledIndex[compareIdx]].correct = true
-//                colors[shuffledIndex[idx]].correct = true
-//            }
-//            else {
-//                print("No answer")
-//                colors[shuffledIndex[compareIdx]].flipped = false
-//                colors[shuffledIndex[idx]].flipped = false
-//            }
-//            flipedCount = 0
-////            checkAnswer()
-//        }
-//        else if (flipedCount == 3) {
-//            flipedCount = 0
-//        }
-        
-        
-//        print("KKKKK", flipedCount, color[0].0, color[0].answer, color[0].2)
-//        if ( flipedCount == 2 ) {
-//            canBeFliped = false
-//        }
-        
     }
     
-    func unFlipCard () {
-        if (flipedCount > 0) {
-            flipedCount -= 1
-        } else if (flipedCount == 0) {
-//            flipedCount
+    func firstCardFlipped (idx:Int) {
+        colors[shuffledIndex[idx]].flipped = true
+        compareIdx = idx
+    }
+    
+    func secondCardFlipped (idx: Int) {
+        colors[shuffledIndex[idx]].flipped = true
+        canBeFliped = false
+        if (colors[shuffledIndex[idx]].answer == colors[shuffledIndex[compareIdx]].answer) {
+            colors[shuffledIndex[idx]].correct = true
+            colors[shuffledIndex[compareIdx]].correct = true
+            canBeFliped = true
+            flipedCount = 0
+        } else {
+            unflip(idx: idx, isCanBeFliped: false)
+            unflip(idx: compareIdx, isCanBeFliped: true)
         }
     }
     
