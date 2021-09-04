4197ff5a91c9a529339222dfe22150a9f035c629
james <junyoungcat@gmail.com>
2021-09-04 17:14:54 +0900

    Create tetris board

--- miniGame/tetris/TetrisGameView.swift
+++ miniGame/tetris/TetrisGameView.swift
@@ -12,12 +12,43 @@ struct TetrisGameView: View {
     @ObservedObject var tetrisGame = TetrisGameViewModel()
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { (geometry: GeometryProxy) in
+//                .background(Color.red)
+            drawBoard(boundingRect: geometry.size)
+        }
+        .navigationBarHidden(true)
     }
+    
+    func drawBoard(boundingRect: CGSize) -> some View {
+        let columns = tetrisGame.numColumns
+        let rows = tetrisGame.numRows
+        let blockSize = min(boundingRect.width/CGFloat(columns), boundingRect.height/CGFloat(rows))
+        let xoffset = (boundingRect.width - blockSize*CGFloat(columns)) / 2
+        let yoffset = (boundingRect.height - blockSize*CGFloat(rows)) / 2
+        
+        return ForEach(0...columns-1, id:\.self) { (column:Int) in
+            ForEach(0...rows-1, id:\.self) { (row:Int) in
+                Path { path in
+                    let x = xoffset + blockSize*CGFloat(column)
+                    let y = boundingRect.height - yoffset - blockSize*CGFloat(row+1)
+                    
+                    let rect = CGRect(x: x, y: y, width: blockSize, height: blockSize)
+                    path.addRect(rect)
+                }
+                .fill(tetrisGame.gameBoard[column][row].color)
+                
+            }
+            
+        }
+    }
+    
 }
 
 struct TetrisGameView_Previews: PreviewProvider {
     static var previews: some View {
-        TetrisGameView()
+        Group {
+            TetrisGameView()
+            TetrisGameView()
+        }
     }
 }
