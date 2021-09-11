79ff24a00d7c2a07c64d7e075e2d8e21601163b3
james <junyoungcat@gmail.com>
2021-09-11 09:48:16 +0900

    Create model in viewModel class

--- miniGame/Memorize/EmojiMemoryGame.swift
+++ miniGame/Memorize/EmojiMemoryGame.swift
@@ -9,4 +9,12 @@ import SwiftUI
 
 class EmojiMemoryGame {
     static let emojis = ["🚗", "🚌", "🚃", "⛴", "🚙", "🚁", "🚕", "🏎", "🚚", "🚛", "✈️", "🛳", "🚀", "🚔", "🛸", "🚟", "🛩", "🚜", "🚝", "🚇", "🛫", "⛵️"]
+    
+    private var model: MemoryGame<String> = MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
+        emojis[pairIndex]
+    }
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
 }
