34cb7c816211bd5c8d90439cf6406ab3a9bc7341
james <junyoungcat@gmail.com>
2021-09-11 10:21:26 +0900

    Create card view

--- miniGame/Memorize/MemorizeView.swift
+++ miniGame/Memorize/MemorizeView.swift
@@ -8,16 +8,39 @@
 import SwiftUI
 
 struct MemorizeView: View {
+    let viewModel: EmojiMemoryGame
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
+struct CardView: View {
+    let card: MemoryGame<String>.Card
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if card.isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 3)
+                Text(card.content).font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            print("Tapped!")
+        }
+    }
+}
+ 
 struct MemorizeView_Previews: PreviewProvider {
     static var previews: some View {
-        MemorizeView()
+        let game = EmojiMemoryGame()
+        MemorizeView(viewModel: game)
             .preferredColorScheme(.light)
-        MemorizeView()
+        MemorizeView(viewModel: game)
             .preferredColorScheme(.dark)
     }
 }
