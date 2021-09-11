e4f29c80024a9a4d2ee2c3a41a9123ad8dec3586
james <junyoungcat@gmail.com>
2021-09-11 09:26:09 +0900

    Add init function for create pair of cards

--- miniGame/Memorize/MemoryGame.swift
+++ miniGame/Memorize/MemoryGame.swift
@@ -14,6 +14,17 @@ struct MemoryGame<CardContent> {
         
     }
     
+    init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
+        cards = Array<Card>()
+        
+        // add numberofPairsOfCards x 2 cards to cards array
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = createCardContent(pairIndex)
+            cards.append(Card(content: content, id: pairIndex*2))
+            cards.append(Card(content: content, id: pairIndex*2 + 1))
+        }
+    }
+    
     struct Card: Identifiable {
         var isFaceUp: Bool = true
         var isMatched: Bool = false
