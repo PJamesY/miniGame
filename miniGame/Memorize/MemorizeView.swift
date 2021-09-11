292493239fe38e09cb03ccb40d53efa56a4ddded
james <junyoungcat@gmail.com>
2021-09-11 10:07:30 +0900

    Create Memorize View

---
+++ miniGame/Memorize/MemorizeView.swift
@@ -0,0 +1,23 @@
+//
+//  MemorizeView.swift
+//  miniGame
+//
+//  Created by james on 2021/09/11.
+//
+
+import SwiftUI
+
+struct MemorizeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MemorizeView_Previews: PreviewProvider {
+    static var previews: some View {
+        MemorizeView()
+            .preferredColorScheme(.light)
+        MemorizeView()
+            .preferredColorScheme(.dark)
+    }
+}
