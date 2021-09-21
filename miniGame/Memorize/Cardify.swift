d86433d4b5dd0db8dca5024bb8becdfafbc1ccfe
james <junyoungcat@gmail.com>
2021-09-21 22:33:27 +0900

    Add rotation 3d effect

--- miniGame/Memorize/Cardify.swift
+++ miniGame/Memorize/Cardify.swift
@@ -22,6 +22,10 @@ struct Cardify: ViewModifier {
             content
                 .opacity(isFaceUp ? 1 : 0)
         }
+        .rotation3DEffect(
+            Angle(degrees: (isFaceUp ? 0 : 180)),
+            axis: /*@START_MENU_TOKEN@*/(x: 0.0, y: 1.0, z: 0.0)/*@END_MENU_TOKEN@*/
+        )
     }
     
     private struct DrawingConstants {
