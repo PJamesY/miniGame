5598fb5e15f1e2c8e14d4a0dda84d8842aeb5f6b
james <junyoungcat@gmail.com>
2021-09-26 21:14:10 +0900

    Change isFaceUp boolean value to true

--- miniGame/Memorize/MemoryGame.swift
+++ miniGame/Memorize/MemoryGame.swift
@@ -51,7 +51,7 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
     }
     
     struct Card: Identifiable {
-        var isFaceUp = false
+        var isFaceUp = true
         var isMatched = false
         let content: CardContent // never changed
         let id: Int // never changed
