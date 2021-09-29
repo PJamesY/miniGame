893733bdc51382832a6a9d5f91e94e340b09c9e3
james <junyoungcat@gmail.com>
2021-09-29 21:40:01 +0900

    Add bonus point

--- miniGame/Memorize/MemoryGame.swift
+++ miniGame/Memorize/MemoryGame.swift
@@ -55,36 +55,42 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
         var isMatched = false
         let content: CardContent // never changed
         let id: Int // never changed
-    }
-    
-    // MARK: - Bonus Time
-    
-    // this could give matching bonus points
-    // if the user matches the card
-    // before a certain amount of time passess during which the card is face up
-    
-    // can be zero which means "no bonus available" for this card
-    var bonusTimeLimit: TimeInterval = 6
-    
-    // how long this card has ever been face up
-    private var faceUpTime: TimeInterval {
-        if let lastFaceUpDate = self.lastFaceUpDate {
-            return pastFaceUpTime + Date().timeIntervalSince(lastFaceUpDate)
-        } else {
-            return pastFaceUpTime
+        
+        // MARK: - Bonus Time
+        
+        // this could give matching bonus points
+        // if the user matches the card
+        // before a certain amount of time passess during which the card is face up
+        
+        // can be zero which means "no bonus available" for this card
+        var bonusTimeLimit: TimeInterval = 6
+        
+        // how long this card has ever been face up
+        private var faceUpTime: TimeInterval {
+            if let lastFaceUpDate = self.lastFaceUpDate {
+                return pastFaceUpTime + Date().timeIntervalSince(lastFaceUpDate)
+            } else {
+                return pastFaceUpTime
+            }
+        }
+        
+        // the last time this card was turned face up (and is still face up)
+        var lastFaceUpDate: Date?
+        // the accumulated time this card has been face up in the past
+        // (i.e. not including the current time it's been face up if it is currently so)
+        var pastFaceUpTime: TimeInterval = 0
+        
+        // how much time left before the bonus opportunity runs out
+        var bonusTimeRemaining: TimeInterval {
+            max(0, bonusTimeLimit - faceUpTime)
+        }
+        // whether the card was matched during the bonus time period
+        var hasEarnedBonus: Bool {
+            isMatched && bonusTimeRemaining > 0
         }
     }
     
-    // the last time this card was turned face up (and is still face up)
-    var lastFaceUpDate: Date?
-    // the accumulated time this card has been face up in the past
-    // (i.e. not including the current time it's been face up if it is currently so)
-    var pastFaceUpTime: TimeInterval = 0
     
-    // how much time left before the bonus opportunity runs out
-    var bonusTimeRemaining: TimeInterval {
-        max(0, bonusTimeLimit - faceUpTime)
-    }
     
 }
 
