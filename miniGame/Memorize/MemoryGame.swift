fe4a81b5af26e30410fc96e6d1f83c337a679402
james <junyoungcat@gmail.com>
2021-09-29 21:59:05 +0900

    Add didset property

--- miniGame/Memorize/MemoryGame.swift
+++ miniGame/Memorize/MemoryGame.swift
@@ -51,8 +51,20 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
     }
     
     struct Card: Identifiable {
-        var isFaceUp = false
-        var isMatched = false
+        var isFaceUp = false {
+            didSet {
+                if isFaceUp {
+                    startUsingBonusTime()
+                } else {
+                    stopUsingBonusTime()
+                }
+            }
+        }
+        var isMatched = false {
+            didSet {
+                stopUsingBonusTime()
+            }
+        }
         let content: CardContent // never changed
         let id: Int // never changed
         
