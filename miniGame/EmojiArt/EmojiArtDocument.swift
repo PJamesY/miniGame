4ced006a4f9bda6e473948cfccdde53c3371a9ea
james <junyoungcat@gmail.com>
2021-10-07 21:38:52 +0900

    Add emojiArtDocument view

---
+++ miniGame/EmojiArt/EmojiArtDocument.swift
@@ -0,0 +1,16 @@
+//
+//  EmojiArtDocument.swift
+//  miniGame
+//
+//  Created by james on 2021/10/07.
+//
+
+import SwiftUI
+
+class EmojiArtDocument: ObservableObject {
+    @Published private(set) var emojiArt: EmojiArtModel
+    
+    init() {
+        emojiArt = EmojiArtModel()
+    }
+}
