425e0cf0074140dea987f49015a784599c6cc797
james <junyoungcat@gmail.com>
2021-10-16 22:27:24 +0900

    Add struct trail

--- miniGame/Move/MoveView.swift
+++ miniGame/Move/MoveView.swift
@@ -7,9 +7,32 @@
 
 import SwiftUI
 
+struct Trail: Identifiable {
+    var id = UUID()
+    var name: String
+    var image: Image
+}
+
 struct MoveView: View {
+    @State private var dragAmount = CGSize.zero
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        
+        Image("1-1")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 200, height: 200)
+//            .frame(width: 100, height: 100)
+            
+//            .scaledToFit()
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged{ dragAmount = $0.translation }
+//                    .onEnded{ _ in dragAmount =  }
+            )
     }
 }
 
