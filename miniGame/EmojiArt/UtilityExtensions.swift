1e53af2eb28fef837c5e7eb21b7b9bac8be1cc7b
james <junyoungcat@gmail.com>
2021-10-25 22:11:28 +0900

    Add URL extension

--- miniGame/EmojiArt/UtilityExtensions.swift
+++ miniGame/EmojiArt/UtilityExtensions.swift
@@ -27,6 +27,20 @@ extension CGRect {
     }
 }
 
+extension URL {
+    var imageURL: URL {
+        for query in query?.components(separatedBy: "&") ?? [] {
+            let queryComponents = query.components(separatedBy: "=")
+            if queryComponents.count == 2 {
+                if queryComponents[0] == "imgurl", let url = URL(string: queryComponents[1].removingPercentEncoding ?? "") {
+                    return url
+                }
+            }
+        }
+        return baseURL ?? self
+    }
+}
+
 extension Character {
     var isEmoji: Bool {
         // Swift does not have a way to ask if a Character isEmoji
