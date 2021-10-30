ab2ccd890ef99ee57a509f60cb596c33dff89125
james <junyoungcat@gmail.com>
2021-10-30 22:53:13 +0900

    Change to move view

--- miniGame/ContentView.swift
+++ miniGame/ContentView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        EmojiArtDocumentView()
-//        MoveView()
+//        EmojiArtDocumentView()
+        MoveView()
 //        Button {
 //        NavigationView {
 ////            NavigationLink("Word", destination:MemorizeView())
