03a8927c2ed4cf2f83f97ec02d6aae32158796bc
james <junyoungcat@gmail.com>
2021-11-08 21:50:44 +0900

    Add memo class

--- miniGame/EmojiArt/EmojiArtDocument.swift
+++ miniGame/EmojiArt/EmojiArtDocument.swift
@@ -8,7 +8,11 @@
 import SwiftUI
 
 class EmojiArtDocument: ObservableObject {
-    @Published private(set) var emojiArt: EmojiArtModel
+    @Published private(set) var emojiArt: EmojiArtModel {
+        didSet {
+            
+        }
+    }
     
     init() {
         emojiArt = EmojiArtModel()
@@ -20,6 +24,8 @@ class EmojiArtDocument: ObservableObject {
     var background: EmojiArtModel.Background { emojiArt.background }
     
     @Published var backgroundImage: UIImage?
+    
+    
     
     // MARK: - Intent(s)
     
---
+++ miniGame/memo/Model/Memo.swift
@@ -0,0 +1,20 @@
+//
+//  Memo.swift
+//  miniGame
+//
+//  Created by james on 2021/11/08.
+//
+
+import SwiftUI
+
+class Memo: Identifiable, ObservableObject {
+    let id: UUID
+    @Published var content: String
+    let insertDate: Date
+    
+    init(id: UUID = UUID(), content: String, insertDate: Date = Date()) {
+        self.id = id
+        self.content = content
+        self.insertDate = insertDate
+    }
+}
